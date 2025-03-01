73f12c1ce167e226fd4b8b042afc31524f0ebe94
JustNormalProgrammer <testkonto1290@gmail.com>
2025-03-01 17:28:41 +0100

    Implement basic express app

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('node:path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+
+const messages = [
+    {
+      text: "Hi there!",
+      user: "Amando",
+      added: new Date()
+    },
+    {
+      text: "Hello World!",
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
+app.get('/', (req, res) => {
+    res.render('index', {messages: messages});
+})
+app.get('/new', (req, res) => {
+    res.render('form');
+})
+app.post('/new', (req, res) => {
+    const {user, text} = req.body;
+    messages.push({user, text, added: new Date()});
+    res.redirect('/');
+})
+app.get('/message/:id', (req, res) => {
+    const {id} = req.params;
+    console.log(id);
+    res.render('message', {id: id, message: messages[id]});
+})
+
+app.listen(8080, () => {
+    console.log("Port 8080 open");
+});
