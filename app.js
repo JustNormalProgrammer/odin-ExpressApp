ffa78a06f9b1fee7619daea4fbec78936864fc2b
JustNormalProgrammer <testkonto1290@gmail.com>
2025-03-07 17:34:00 +0100

    Add async handler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('node:path');
 const db = require('./db/queries');
+const { error } = require('node:console');
+const asyncHandler = require('express-async-handler');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,26 +12,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', async (req, res) => {
+app.get('/', asyncHandler(async (req, res) => {
     const messages = await db.getMessages();
     res.render('index', {messages: messages});
-})
+}))
 app.get('/new', (req, res) => {
     res.render('form');
 })
-app.post('/new', async (req, res) => {
+app.post('/new', asyncHandler(async (req, res) => {
     const {author, text} = req.body;
     await db.addMessage(author, text, new Date().toISOString().slice(0, 19).replace('T', ' '));
     res.redirect('/');
-})
-app.get('/message/:id', async (req, res) => {
+}))
+app.get('/message/:id', asyncHandler(async (req, res) => {
     const {id} = req.params;
     const message = await db.getMessageById(id);
     res.render('message', {message:message});
-})
+}))
 
 app.use((err, req, res, next) => {
-    res.status(500).send('Error');
+    console.log(err);
+    res.status(500).send('Error!');
 })
 
 app.listen(port, () => {
